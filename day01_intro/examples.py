a2dc7b9a83cd2792d080aa58e7c41f154e42c43a
hsikelias <plekishsai@gmail.com>
2025-05-27 16:03:47 -0400

    Update examples.py

--- day01_intro/examples.py
+++ day01_intro/examples.py
@@ -1,1 +1,20 @@
+print ("Hello World") #prints out Hello World
+# print (Hello World)
 
+print ("6" + "7")  #gives you 67 because python considers the numbers as string
+print ( 6 +7)
+print (6.743 + 6)
+
+# holding multiple characters 
+print ( "hello world", 0) # holding both string and number
+
+
+# time = input("What time is it? ")  
+# try:
+# 	hour = int(time)
+# 	if 1 <= hour <= 12: 
+# 		print("Time is between 1:00 and 12:00") 
+# 	else:
+# 		print("Time is outside 1:00 to 12:00") 
+# except ValueError:
+# 	print("Please enter a valid hour as a number.")   
