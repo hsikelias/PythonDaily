4c7b5d816f760add6135a6efcd90ffc321a47eb2
hsikelias <plekishsai@gmail.com>
2025-06-09 08:32:27 -0400

    skipped few days cus lazy

---
+++ day05_stringsliceandmethods_/exercises5.py
@@ -0,0 +1,19 @@
+name = "Lekish, Podili"
+
+print(name[0:5])
+
+fruit = "Mango"
+len1 = len(fruit)
+print("Mango is a", len1, "letter word.")
+
+
+print(len(name))
+
+print(name[:5])   # python assumes empty is 0
+
+#quick quiz
+nm = "Harry"
+print(nm[-4:-2])
+print(len(nm))
+# so total len of char is  4
+# 0:2 = Har
